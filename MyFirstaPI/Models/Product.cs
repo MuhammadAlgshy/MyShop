73d22faead3951409049e73e72dbcdb6669f1b46
MuhammadAlgshy <m.algshy@live.com>
2022-11-29 14:56:50 +0000

    Add project files.

---
+++ MyFirstaPI/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Build.Framework;
+using Newtonsoft.Json;
+
+namespace MyFirstaPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Sku { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+
+        [JsonIgnore]
+        public virtual Category? Category { get; set; }
+
+
+
+    }
+}
