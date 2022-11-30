c1dc4af9aceba47d7433f86312103380111930c5
MuhammadAlgshy <m.algshy@live.com>
2022-11-30 13:50:14 +0000

    Apply Authentication/Identity

---
+++ Front/Areas/Identity/Data/FrontContext.cs
@@ -0,0 +1,22 @@
+﻿using Front.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Front.Data;
+
+public class FrontContext : IdentityDbContext<FrontUser>
+{
+    public FrontContext(DbContextOptions<FrontContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- Front/Program.cs
+++ Front/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Front.Data;
+using Front.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("FrontContextConnection") ?? throw new InvalidOperationException("Connection string 'FrontContextConnection' not found.");
+
+builder.Services.AddDbContext<FrontContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<FrontUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<FrontContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
